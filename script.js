0fcc7908cf2989ffce3859ba4066e5fa823f134b
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-10-22 02:04:57 +0600

    push

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const tg = window.Telegram.WebApp;
 tg.ready();
-tg.expand(); // попытка увеличить область
+tg.expand();
 
-const user = tg.initDataUnsafe?.user;
-document.getElementById("user-name").innerText = user ? `Здравствуйте, ${user.first_name}!` : "Здравствуйте, гость!";
-document.getElementById("photo-profile").src = user && user.photo_url ? user.photo_url : "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=";
+const user = tg.initDataUnsafe?.user ?? null;
+
+const nameElement = document.getElementById("user-name");
+const photoElement = document.getElementById("photo-profile");
+
+if (user) {
+  nameElement.textContent = `Здравствуйте, ${user.first_name || "гость"}!`;
+  photoElement.src = user.photo_url || "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=";
+} else {
+  nameElement.textContent = "Здравствуйте, гость!";
+  photoElement.src = "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=";
+}
