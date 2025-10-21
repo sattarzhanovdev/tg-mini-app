0dcd21919fca96d8dd6966df728bc1fa950973cd
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-10-22 02:56:12 +0600

    main page finally have finished

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ if (user) {
   nameElement.textContent = "Здравствуйте, гость!";
   photoElement.src = "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=";
 }
+
+const navLinks = document.querySelectorAll("footer .bottom__bar li a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    // снять active со всех
+    navLinks.forEach(l => l.classList.remove("active"));
+
+    // добавить active на выбранный
+    link.classList.add("active");
+  });
+});
